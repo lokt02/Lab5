60c9b3fbbed01bb6c4eb86b84c3997f12565216c
Sergei <lokt02@mail.ru>
2021-12-11 20:45:52 +0300

    tic tac toe

---
+++ Tests/TicTacToeTests.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+#include "../TicTacToe.h"
+
+TEST(TicTacToe_Test, Init){
+    TicTacToe t = TicTacToe();
+}
--- main.cpp
+++ main.cpp
@@ -1,29 +1,13 @@
 #include <iostream>
 #include "ArraySequence.h"
 #include "SequenceSorter.h"
+#include "TicTacToe.h"
 #include <random>
 #include <chrono>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> dist(-1000, 1000);
-    auto array = new ArraySequence<int>();
-    for(int i = 0; i < 10000; i++){
-        array->Append(dist(gen));
-    }
-    auto t1 = std::chrono::high_resolution_clock::now();
-    SequenceSorter<int>::QuickSort(array);
-    auto t2 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, milli> ms_double = t2 - t1;
-    cout << "Result: " << ms_double.count() << "ms" << endl;
-
-    auto t3 = std::chrono::high_resolution_clock::now();
-    SequenceSorter<int>::QuickSort(array);
-    auto t4 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, milli> ms_doubl1 = t4 - t3;
-    cout << "Result: " << ms_doubl1.count() << "ms" << endl;
-
+    TicTacToe t = TicTacToe();
+    t.Build();
+    t.Output();
     return 0;
 }
